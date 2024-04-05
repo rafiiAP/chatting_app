80dd472fa14a7dcbf9892f40b8d8c5cf227d65ca
rafiiAP <prafi726@gmail.com>
2024-04-06 01:38:41 +0700

    update add firebase option

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:chatting_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'page/chat_page.dart';
 import 'page/login_page.dart';
 import 'page/register_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -14,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Dicoding Chatting',
       theme: ThemeData(
         primarySwatch: Colors.blue,
